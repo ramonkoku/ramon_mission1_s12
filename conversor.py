d56e7ae952d32192be3c5dbc0a26a85abe901000
Ramon <kokusilva206@gmail.com>
2024-05-29 18:03:31 -0300

    Inicializa projeto

---
+++ conversor.py
@@ -0,0 +1,15 @@
+def menu_principal():
+    print("\n**calculadora de conversão de unidades**")
+    print("1. converter comprimento")
+    print("3. sair")
+
+    opcao=input("digite A opição desejada: ")
+    return opcao
+
+def sair():
+    print("encerrando o progama...")
+
+escolha =menu_principal()
+
+if escolha =='3':
+    sair()
