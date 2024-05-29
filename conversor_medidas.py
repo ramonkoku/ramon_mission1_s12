3710482acda8a46f0dfd706fdd8295c5ce62c5f8
Ramon <kokusilva206@gmail.com>
2024-05-29 18:26:13 -0300

    criei o convertor de metros para centimetros

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -1,3 +1,11 @@
 class ConvertorMedidas:
     def __init__(self):
         pass
+
+def centimetrosParaMetros(centimetros):
+    metros = centimetros / 100
+    return metros
+
+valor_em_centimetros = 2000
+valor_em_metros = centimetrosParaMetros(valor_em_centimetros)
+print(f"{valor_em_centimetros} centímetros equivalem a {valor_em_metros:.2f} metros.")
