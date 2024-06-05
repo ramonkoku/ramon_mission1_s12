645387c8dae395e2eceaf948f4ee35824fc10131
Ramon <kokusilva206@gmail.com>
2024-06-05 18:28:49 -0300

    fim

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from conversor_medidas import ConvertorMedidas
-
-
 def menu_principal():
     print("\n**calculadora de conversão de unidades**")
-    print("1. converter comprimento")
-    print("3. sair")
+    print("1. converter de comprimento")
+    print("2. converter de temperaturas")
+    print("3. sair ")
 
     opcao=input("digite A opição desejada: ")
     return opcao
-
 def sair():
     print("encerrando o progama...")
+
 
 escolha =menu_principal()
 
 if escolha =='3':
     sair()
+elif escolha == '2':
+    from conversor_temperaturas import conversorTemperaturas
+elif escolha == '1' :
+    from conversor_medidas import ConvertorMedidas
+else :
+    print("vai tomar no cu, é só seguir a regras")
