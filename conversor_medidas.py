bd4b5ee2ed283b7d93742042f575db3d147d7553
Ramon <kokusilva206@gmail.com>
2024-06-05 17:14:59 -0300

    Cria função centrimetrosParaMetros

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -6,6 +6,6 @@ def centimetrosParaMetros(centimetros):
     metros = centimetros / 100
     return metros
 
-valor_em_centimetros = 2000
+valor_em_centimetros = 1000
 valor_em_metros = centimetrosParaMetros(valor_em_centimetros)
 print(f"{valor_em_centimetros} centímetros equivalem a {valor_em_metros:.2f} metros.")
