8219d7e4a79546e6c497a7d926a01e8c9dc2868c
Ramon <kokusilva206@gmail.com>
2024-06-05 18:02:31 -0300

    terminado

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -1,3 +1,20 @@
 class conversorTemperaturas:
     def __init__(self):
         pass
+    
+def celsiusParaFahrenheit(celsius):
+    Fahrenheit = (9/5*celsius)-32
+    return Fahrenheit
+
+valor_em_celsius = 32
+valor_em_Fahrenheit = celsiusParaFahrenheit(valor_em_celsius)
+print(f"{valor_em_celsius} celsius equivalem a {valor_em_Fahrenheit:.2f} Fahrenheit.")
+
+def centimetrosParaFahrenheit(Fahrenheit):
+    celsius = 5/9 *(Fahrenheit - 32)
+    return celsius
+
+valor_em_Fahrenheit = 57
+valor_em_celsius = centimetrosParaFahrenheit(valor_em_Fahrenheit)
+print(f"{valor_em_Fahrenheit} Fahrenheit equivalem a {valor_em_celsius:.2f} celsius.")
+
