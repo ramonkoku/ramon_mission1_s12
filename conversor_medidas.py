2b0c98934722dc6722a31bca8a3eb17a04fa5da4
Ramon <kokusilva206@gmail.com>
2024-06-05 17:35:20 -0300

    coloquei a nova funçao

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -9,3 +9,12 @@ def centimetrosParaMetros(centimetros):
 valor_em_centimetros = 1000
 valor_em_metros = centimetrosParaMetros(valor_em_centimetros)
 print(f"{valor_em_centimetros} centímetros equivalem a {valor_em_metros:.2f} metros.")
+
+def metrosParaMetros(metros):
+    centimetros = metros * 100
+    return centimetros
+
+valor_em_metros = 100
+valor_em_centimetros = centimetrosParaMetros(valor_em_metros)
+print(f"{valor_em_metros} metros equivalem a {valor_em_centimetros:.2f} centimetros.")
+
